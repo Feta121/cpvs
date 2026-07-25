20ce3cd2a6997a6b07981f67e0048a227a7c7c78
Abdulfetah Kedir <abdulfetahkedir6@gmail.com>
2026-07-25 19:09:26 +0300

    Update mark-absences function

---
+++ android/app/src/main/java/com/aau/cpvs/MainActivity.java
@@ -0,0 +1,5 @@
+package com.aau.cpvs;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aau.cpvs',
+  appName: 'cpvs',
+  webDir: 'dist'
+};
+
+export default config;
